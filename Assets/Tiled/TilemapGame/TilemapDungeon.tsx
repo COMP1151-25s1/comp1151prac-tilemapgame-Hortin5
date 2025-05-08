0cfb51a8296b5b8e419ad88c53f11f7bed03f481
Hortin5 <austin.whitfield@students.mq.edu.au>
2025-05-08 14:58:54 +1000

    Collisions

---
+++ Assets/Tiled/TilemapGame/TilemapDungeon.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TilemapDungeon" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
+ <image source="../tilemap.png" width="203" height="186"/>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.90789" y="0" width="14.0921" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="14.0921" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="5" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.84211" y="0" width="14.1579" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="13.7632" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
