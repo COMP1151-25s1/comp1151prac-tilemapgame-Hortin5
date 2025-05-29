9358b7a0b57e14348e82c12e6ad7e810124bbd13
Hortin5 <austin.whitfield@students.mq.edu.au>
2025-05-29 14:30:54 +1000

    Tile Changes

--- Assets/Tiled/TilemapGame/TilemapDungeon.tsx
+++ Assets/Tiled/TilemapGame/TilemapDungeon.tsx
@@ -1,6 +1,25 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="TilemapDungeon" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
+ <properties>
+  <property name="unity:IsTrigger" value="true"/>
+  <property name="unity:layer" value="Hazard"/>
+ </properties>
  <image source="../tilemap.png" width="203" height="186"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1.6875" width="16" height="14.3125"/>
+  </objectgroup>
+ </tile>
  <tile id="16">
   <objectgroup draworder="index" id="2">
    <object id="1" x="1.90789" y="0" width="14.0921" height="16"/>
@@ -11,19 +30,36 @@
    <object id="1" x="0" y="0" width="14.0921" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1.375" width="16" height="14.625"/>
+  </objectgroup>
+ </tile>
  <tile id="40">
   <objectgroup draworder="index" id="2">
    <object id="5" x="0" y="0" width="16" height="16"/>
+   <object id="6" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="57">
   <objectgroup draworder="index" id="2">
    <object id="1" x="1.84211" y="0" width="14.1579" height="16"/>
+   <object id="2" x="1.75" y="0" width="14.25" height="16"/>
   </objectgroup>
  </tile>
  <tile id="59">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="13.7632" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="89">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2.0625" width="16" height="13.9375"/>
   </objectgroup>
  </tile>
 </tileset>
